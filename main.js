05b6b0e5bae731146acfe71caf629bd3c44b8c93
Edwin Körner <edwin.korner@medieinstitutet.se>
2021-11-15 22:46:07 +0100

    Börjat med startsidan

---
+++ main.js
@@ -0,0 +1,15 @@
+let namn = prompt("Skriv in ditt namn");
+document.getElementById("namn").innerHTML = namn;
+
+function startaspelet(){
+    let h2 = document.getElementById('main-h2');
+    h2.remove();
+
+    let button = document.getElementById('starta-spelet');
+    button.remove();
+
+    document.getElementById("välj-kamera").style.display = "block";
+}
+
+let scener = ['cam1', 'cam3', 'cam7'];
+
