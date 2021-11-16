7545a4778a7ab6a1541a31c137298c86569917ff
Edwin Körner <edwin.korner@medieinstitutet.se>
2021-11-16 15:58:43 +0100

    Har fixat alla fel med scenbytet på ett fult sätt

--- main.js
+++ main.js
@@ -13,33 +13,39 @@ function startaspelet(){
 
 let scener = ['cam1', 'cam3', 'cam7'];
 
-function cam1(){
+document.getElementById("cam1").addEventListener("click", cam1);
 
+function cam1(){
+    document.getElementById("backrooms1").style.display="block";
+    document.getElementById("backrooms2").style.display="none";
+    document.getElementById("backrooms3").style.display="none";
     document.getElementById("knappar").style.bottom ="5rem";
     document.getElementById("static").style.display="block";
-    document.getElementById("backrooms1").style.display="block";
     document.getElementById("cam1").style.display="none";
     document.getElementById("cam3").style.display="block";
     document.getElementById("cam7").style.display="block";
 }
 
+document.getElementById("cam3").addEventListener("click", cam3);
+
 function cam3(){
 
-    document.getElementById("knappar").style.bottom ="5rem";
-    document.getElementById("static").style.display="block";
+    document.getElementById("backrooms1").style.display="none";
     document.getElementById("backrooms2").style.display="block";
+    document.getElementById("backrooms3").style.display="none";
     document.getElementById("cam1").style.display="block";
     document.getElementById("cam3").style.display="none";
     document.getElementById("cam7").style.display="block";
 }
 
+document.getElementById("cam7").addEventListener("click", cam7);
+
 function cam7(){
 
-    document.getElementById("knappar").style.bottom ="5rem";
-    document.getElementById("static").style.display="block";
+    document.getElementById("backrooms1").style.display="none";
+    document.getElementById("backrooms2").style.display="none";
     document.getElementById("backrooms3").style.display="block";
     document.getElementById("cam1").style.display="block";
     document.getElementById("cam3").style.display="block";
     document.getElementById("cam7").style.display="none";
-
 }
