47131690a089602bee7021704745d2e29b5a8f8b
Edwin Körner <edwin.korner@medieinstitutet.se>
2021-11-18 15:53:53 +0100

    Har gjort js filen lite mer läsbar och börjat fortsätta med andra funktioner

--- main.js
+++ main.js
@@ -13,46 +13,55 @@ function startaspelet(){
 
 let scener = ['cam1', 'cam3', 'cam7'];
 
-document.getElementById("cam1").addEventListener("click", cam1);
+document.getElementById("cam1").addEventListener("click", kamera1);
 
-function cam1(){
-    document.getElementById("backrooms1").style.display="block";
-    document.getElementById("backrooms2").style.display="none";
-    document.getElementById("backrooms3").style.display="none";
-    document.getElementById("knappar").style.bottom ="5rem";
-    document.getElementById("static").style.display="block";
-    document.getElementById("cam1").style.display="none";
-    document.getElementById("cam3").style.display="block";
-    document.getElementById("cam7").style.display="block";
+//Variabler för att göra mer läsbara funtioner
+
+let securityCam1 = document.getElementById("security-cam1");
+let securityCam3 = document.getElementById("security-cam3");
+let securityCam7 =  document.getElementById("security-cam7");
+let knappar = document.getElementById("knappar");
+let static = document.getElementById("static");
+let cam1 = document.getElementById("cam1");
+let cam3 = document.getElementById("cam3");
+let cam7 = document.getElementById("cam7");
+
+function kamera1(){
+    securityCam1.style.display="block";
+    securityCam3.style.display="none";
+    securityCam7.style.display="none";
+    knappar.style.bottom ="5rem";
+    cam1.style.display="none";
+    cam3.style.display="block";
+    cam7.style.display="block";
 }
 
-document.getElementById("cam3").addEventListener("click", cam3);
+document.getElementById("cam3").addEventListener("click", kamera3);
 
-function cam3(){
+function kamera3(){
 
-    document.getElementById("backrooms1").style.display="none";
-    document.getElementById("backrooms2").style.display="block";
-    document.getElementById("backrooms3").style.display="none";
-    document.getElementById("knappar").style.bottom ="5rem";
-    document.getElementById("static").style.display="block";
-    document.getElementById("cam1").style.display="block";
-    document.getElementById("cam3").style.display="none";
-    document.getElementById("cam7").style.display="block";
+    securityCam1.style.display="none";
+    securityCam3.style.display="block";
+    securityCam7.style.display="none";
+    knappar.style.bottom ="5rem";
+    cam1.style.display="block";
+    cam3.style.display="none";
+    cam7.style.display="block";
 }
 
-document.getElementById("cam7").addEventListener("click", cam7);
+document.getElementById("cam7").addEventListener("click", kamera7);
 
-function cam7(){
+function kamera7(){
 
-    document.getElementById("backrooms1").style.display="none";
-    document.getElementById("backrooms2").style.display="none";
-    document.getElementById("backrooms3").style.display="block";
-    document.getElementById("knappar").style.bottom ="5rem";
-    document.getElementById("static").style.display="block";
-    document.getElementById("cam1").style.display="block";
-    document.getElementById("cam3").style.display="block";
-    document.getElementById("cam7").style.display="none";
-    setTimeout(function(){document.getElementById("malfunction").style.display="block";}, 2000);
+    securityCam1.style.display="none";
+    securityCam3.style.display="none";
+    securityCam7.style.display="block";
+    knappar.style.bottom ="5rem";
+    cam1.style.display="block";
+    cam3.style.display="block";
+    cam7.style.display="none";
+    setTimeout(function(){document.getElementById("malfunction").style.display="block";}, 15000);
+    setTimeout(function(){document.getElementById("fixaKamera").style.display="block";}, 20000);
 }
 
 
